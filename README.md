0624c1b63319c28d0add86b6d7e75a620afe36c1
ST LEON <leonst998@gmail.com>
2015-04-01 16:37:20 +0300

    binary search

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 Что внутри:
 
 - [Линейный поиск](http://omgit.ru/blog/linear-search/)
+- [Бинарный поиск](http://omgit.ru/blog/binary-search/)
--- tests.py
+++ tests.py
@@ -1,4 +1,5 @@
 from linear_search import linear_search_1, linear_search_2, linear_search_3
+from binary_search import binary_search
 import unittest
 
 class TestLinearSearch(unittest.TestCase):
@@ -19,6 +20,30 @@ class TestLinearSearch(unittest.TestCase):
 
 	def testEnd(self):
 		self.assertFunctions(linear_search_1, linear_search_2, linear_search_3, str2find='p', index2find=9)
+
+
+class TestBinarySearch(unittest.TestCase):
+
+	def testDuplicateElements(self):
+		'''
+		Точно не знаю, должна ли функция возвращать результат 0 или 1. В данный ситуации вернет 1, 
+		но можно дописать в коде условие, чтобы проверяло, не является ли элемент слева таким же и тд. 
+		И тогда вернуть крайний левый похожий элемент.
+		'''
+		self.assertEqual(binary_search([1, 1, 2, 2, 3, 3, 4], 1), 1, 'Problem with duplicate elements')
+
+	def testListLen(self):
+		self.assertEqual(binary_search([], 1), None, 'Problem with empty list')
+		self.assertEqual(binary_search([5], 5), 0, 'Problem with one element in list')
+		self.assertEqual(binary_search([5, 6], 6), 1, 'Problem with 2 elements in list')
+
+	def testFirstLastElements(self):
+		self.assertEqual(binary_search([0, 1, 2, 3, 4, 5], 0), 0, 'Problem with first element in the list')
+		self.assertEqual(binary_search([0, 1, 2, 3, 4, 5], 5), 5, 'Problem with last element in the list')
+
+	def testNotIn(self):
+		self.assertEqual(binary_search([1, 2], 3), None, 'Element not in list')
+
 
 
 if __name__ == '__main__':
